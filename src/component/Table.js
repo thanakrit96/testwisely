bd3485a0f731f61826017b125abf01e5abd3de9c
thanakrit96 <thanakrit.c58@gmail.com>
2023-01-30 12:03:58 +0700

    addjob

--- src/component/Table.js
+++ src/component/Table.js
@@ -4,7 +4,62 @@ import './Table.css'
 export default function Table() {
   const [users, setUsers] = React.useState([]);
   const f = async () => {
-    const res = await fetch("https://reqres.in/api/users/");
+    const res = await fetch('https://reqres.in/api/users/',{
+      method:'post',
+      headers:{
+        'Content-Type':'application/json'
+      },body:JSON.stringify({
+        
+          "page": 1,
+          "per_page": 6,
+          "total": 12,
+          "total_pages": 2,
+          "data": [
+              {
+                  "id": 1,
+                  "email": "Co-Founder/CEO",
+                  "first_name": "George",
+                  "avatar": "https://reqres.in/img/faces/1-image.jpg"
+              },
+              {
+                  "id": 2,
+                  "email": "Co-Founder/CTO",
+                  "first_name": "Janet",
+                  "avatar": "https://reqres.in/img/faces/2-image.jpg"
+              },
+              {
+                  "id": 3,
+                  "email": "Manager,Business Relations",
+                  "first_name": "Emma",
+                  "avatar": "https://reqres.in/img/faces/3-image.jpg"
+              },
+              {
+                  "id": 4,
+                  "email": "Font-end Developer",
+                  "first_name": "Eve",
+                  "avatar": "https://reqres.in/img/faces/4-image.jpg"
+              },
+              {
+                  "id": 5,
+                  "email": "Designer",
+                  "first_name": "Charles",
+                  "avatar": "https://reqres.in/img/faces/5-image.jpg"
+              },
+              {
+                  "id": 6,
+                  "email": "Director,Product Development",
+                  "first_name": "Tracey",
+                  "avatar": "https://reqres.in/img/faces/6-image.jpg"
+              }
+          ],
+          "support": {
+              "url": "https://reqres.in/#support-heading",
+              "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+          },
+          "updatedAt": "2023-01-30T04:07:16.190Z"
+      
+      })
+    });
     const json = await res.json();
     setUsers(json.data);
   };
@@ -24,8 +79,8 @@ export default function Table() {
               <div className="infocon" key={user.id}>
                 <img key={user.avatar} src={user.avatar} />
                 <div>
-                  <p>{user.first_name}</p>
-                  {/* <p>{user.email}</p> */}
+                  <div>{user.first_name}</div>
+                  <div className="jobl">{user.email}</div>
                 </div>                             
               </div>
             );
--- src/component/TableA.js
+++ src/component/TableA.js
@@ -5,10 +5,65 @@ import { SiTwitter,SiLinkedin } from "react-icons/si";
 export default function Table() {
   const [users, setUsers] = React.useState([]);
   const f = async () => {
-    const res = await fetch("https://reqres.in/api/users/").then(function (response){
+    const res = await fetch('https://reqres.in/api/users/',{
+      method:'post',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        "page": 1,
+          "per_page": 6,
+          "total": 12,
+          "total_pages": 2,
+          "data": [
+              {
+                  "id": 1,
+                  "email": "Copywriter",
+                  "first_name": "George",
+                  "avatar": "https://reqres.in/img/faces/1-image.jpg"
+              },
+              {
+                  "id": 2,
+                  "email": "Senior Designer",
+                  "first_name": "Janet",
+                  "avatar": "https://reqres.in/img/faces/2-image.jpg"
+              },
+              {
+                  "id": 3,
+                  "email": "Manager,Business Relations",
+                  "first_name": "Principal Designer",
+                  "avatar": "https://reqres.in/img/faces/3-image.jpg"
+              },
+              {
+                  "id": 4,
+                  "email": "VIP,User Exoeriance",
+                  "first_name": "Eve",
+                  "avatar": "https://reqres.in/img/faces/4-image.jpg"
+              },
+              {
+                  "id": 5,
+                  "email": "VP,Human Resources",
+                  "first_name": "Charles",
+                  "avatar": "https://reqres.in/img/faces/5-image.jpg"
+              },
+              {
+                  "id": 6,
+                  "email": "Senioer Font-end Developer",
+                  "first_name": "Tracey",
+                  "avatar": "https://reqres.in/img/faces/6-image.jpg"
+              }
+          ],
+          "support": {
+              "url": "https://reqres.in/#support-heading",
+              "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+          },
+          "updatedAt": "2023-01-30T04:07:16.190Z"
+      })
+    })
+      .then(function (response){
         return response.json()        
     })
-      console.log(res) 
+    console.log(res) 
      res.data.sort(compare) 
      console.log(res) 
     setUsers(res.data)
@@ -39,7 +94,10 @@ export default function Table() {
             return (
               <div className="infocons" key={user.id}>
                 <img key={user.avatar} src={user.avatar} />
-                <div className="twli"><p>{user.first_name}</p>
+                <div className="twli">
+                  <div className="name">{user.first_name}</div>
+                  <div className="jobM">{user.email}</div>
+                     
                      <div className="tw">
                       <div className="twii"><SiTwitter/></div>
                       <div className="liin"><SiLinkedin/></div>
